d273061012b468752531e55e07e2a5e348a422fd
Connor Dailey <connorldailey@gmail.com>
2024-06-26 16:15:14 -0400

    Initial Commit

---
+++ WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Connor Dailey on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
